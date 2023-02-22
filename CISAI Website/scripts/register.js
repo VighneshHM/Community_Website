c31abf6ad3b08593ab99269a142d5a41295968f4
Vighnesh Haresh Madani <vighneshcochin@gmail.com>
2023-02-22 20:25:07 +0530

    ...

--- CISAI Website/scripts/register.js
+++ CISAI Website/scripts/register.js
@@ -1,5 +1,5 @@
 document.getElementById("submit").addEventListener('click', function () {
     console.log('Button clicked');
     window.btoa(document.getElementById("photo").value);
-    window.location = '../index.html';
+    window.location = '../loads/regSuccess.html';
 });
