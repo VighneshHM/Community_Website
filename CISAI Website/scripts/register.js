d5ef6cc6f4270a3e55531a5c19cbeefdbdbf0982
Vighnesh Haresh Madani <vighneshcochin@gmail.com>
2023-02-24 15:17:36 +0530

    form modifs...

--- CISAI Website/scripts/register.js
+++ CISAI Website/scripts/register.js
@@ -1,5 +1,27 @@
 document.getElementById("submit").addEventListener('click', function () {
     console.log('Button clicked');
-    window.btoa(document.getElementById("photo").value);
-    // window.location = '../loads/regSuccess.html';
+    // window.btoa(document.getElementById("photo").value);
+
+    var data = new FormData();
+    jQuery.each(jQuery('#file')[0].files, function(i, file) {
+        data.append('file-'+i, file);
+    });
+    jQuery.ajax({
+        url: 'https://script.google.com/macros/s/AKfycbwMtgyVZwt10T_DPWC8XcKXxI3ZQGTSJQYPwTEpmAGvoAcoHeR9YdeBC9Ri3vUBN7du/exec',
+        data: data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        method: 'POST',
+        type: 'POST', // For jQuery < 1.9
+        success: function(){
+            window.location = '../loads/regSuccess.html';
+        },
+        error: function(textStatus){
+            alert( "Regestration failed: " + textStatus );
+        }
+    }).done(function() {
+        $(this).addClass('done');
+        window.location = '../loads/regSuccess.html';
+    })
 });
